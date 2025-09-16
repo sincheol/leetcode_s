eba9a216d51722b243e47a1fb854bf6e1d5885a2
sincheol <ohmong0803@gmail.com>
2025-09-16 16:36:41 +0900

    t

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -4,8 +4,10 @@ from typing import Tuple
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from base64 import b64encode, b64decode
-
-
+'''
+함수 모음
+'''
+d
 def pad(text:str)->str:
     return text + (16 - len(text) % 16) * chr(16 - len(text) % 16)
 
